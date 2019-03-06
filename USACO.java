fe69ee445beab4cee8fc267660d523129b6f4bf4
Brandon <bchen00@stuy.edu>
2019-03-06 10:13:45 -0500

    able to read a file

---
+++ USACO.java
@@ -0,0 +1,16 @@
+public class USACO{
+	public static int bronze(String filename){
+	File text = new File(filename);
+	Scanner inf1 = Scanner(text);
+	Scanner inf2 = Scanner(text);
+	int col = 0;
+	int row = 0;
+	String line = inf1.nextLine();
+	row = line.charAt(0);
+	col = line.charAt(1);
+	int[][] maze = new int[row][col]
+	for (int r = 0; r < row; r++){
+		String
+	}
+
+}
