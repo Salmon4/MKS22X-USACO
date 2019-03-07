9971d9cb08915abb9a9c1c9f2b20ccbe1f2fc78a
Brandon <bchen00@stuy.edu>
2019-03-07 09:48:46 -0500

    fixed reading issues

--- USACO.java
+++ USACO.java
@@ -1,20 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class USACO{
-	public static int bronze(String filename){
+	public static void bronze(String filename) throws FileNotFoundException{
 	File text = new File(filename);
-	Scanner inf1 = Scanner(text);
-	Scanner inf2 = Scanner(text);
+	Scanner inf1 = new Scanner(text);
+	Scanner inf2 = new Scanner(text);
 	int col = 0;
 	int row = 0;
 	String line = inf1.nextLine();
-	row = line.charAt(0);
-	col = line.charAt(1);
+	row = Integer.parseInt(inf1.next());
+	col = Integer.parseInt(inf1.next());
+	inf1.next();
+	inf1.next();
+	//row = line.charAt(0);
+	//col = line.charAt(1);
 	//int end = line.charAt(line.length()-1);
-	int[][] field = new int[row][col]
+	int[][] field = new int[row][col];
 	for (int r = 0; r < row; r++){
-		String line = inf1.nextLine();
+		//line = inf1.nextLine();
 		for (int c = 0; c < col; c++){
-			field[r][c] = line.charAt(a);
+			//field[r][c] = line.next();
+			field[r][c] = Integer.parseInt(inf1.next());
 		}
 	}
+	}
+
+	
 
 }
