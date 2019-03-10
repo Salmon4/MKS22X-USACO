3a522395fa34b905c301ef27679b26336da07727
Brandon Chen <bchen00@stuy.edu>
2019-03-09 20:42:08 -0500

    got rid of debugging stuff

--- USACO.java
+++ USACO.java
@@ -2,9 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class USACO{
-//	private int[][] field;
-	//private int[] herd = new int[9];
-	//private boolean[] finish = new boolean[9];
 	public static int bronze(String filename) throws FileNotFoundException{
 		int[][]field;
 		int[] herd = new int[9];
@@ -14,30 +11,26 @@ public class USACO{
 		int col = 0;
 		int row = 0;
 		int d;
-		//String line; //inf1.nextLine();
+
 		row = Integer.parseInt(inf1.next());
 		col = Integer.parseInt(inf1.next());
-		//System.out.println("" + row + " " + col);
+
 		d = Integer.parseInt(inf1.next());
 		Integer.parseInt(inf1.next());
-		//line = inf1.nextLine();
-		//line = inf1.nextLine();
+
 		field = new int[row][col];
 		for (int r = 0; r < row; r++){
 			for (int c = 0; c < col; c++){
 				field[r][c] = Integer.parseInt(inf1.next());
 			}
-			//line = inf1.nextLine();
 		}
-		//for (int i = 0; i < 9; i++){
-		//	i = false;
-		//}
+
 		while (inf1.hasNext()){
 		bronzeHelper(Integer.parseInt(inf1.next())-1,
 			Integer.parseInt(inf1.next())-1,
 			Integer.parseInt(inf1.next()),
 			herd,field);
-			//inf1.nextLine();
+
 		}
 		int temp = 0;
 		for (int r = 0; r < field.length; r++){
@@ -81,13 +74,6 @@ System.out.println(ans);
 		return max;
 	}
 	private static boolean bronzeHelper(int row, int col, int depth,int[] herd, int[][] field){
-	//	System.out.println("" + row + " " + col);
-
-		//if (depth == 0){
-	//		return true;
-	//	}
-
-
 		while(depth != 0){
 			herd[0] = field[row][col];
 			herd[1] = field[row][col+1];
@@ -111,38 +97,7 @@ System.out.println(ans);
 	}
 	return true;
 	}
-		/**
-		if (check(0, herd) && field[row-1][col-1] != 0){
-			field[row-1][col-1] -= 1;
-		}
-		if (check(1, herd) && field[row-1][col] != 0){
-			field[row-1][col] -= 1;
-		}
-		if (check(2, herd) && field[row-1][col+1] != 0){
-			field[row-1][col+1] -= 1;
-		}
-		if (check(3, herd) && field[row][col-1] != 0){
-			field[row][col-1] -= 1;
-		}
-		if (check(4, herd) && field[row][col] != 0){
-			field[row][col] -= 1;
-		}
-		if (check(5, herd) && field[row][col+1] != 0){
-			field[row][col+1] -= 1;
-		}
-		if (check(6, herd) && field[row+1][col-1] != 0){
-			field[row+1][col-1] -= 1;
-		}
-		if (check(7, herd) && field[row+1][col] != 0){
-			field[row+1][col] -= 1;
-		}
-		if (check(8, herd) && field[row+1][col+1] != 0){
-			field[row+1][col+1] -= 1;
-		}
-		return bronzeHelper(row,col,depth--,herd,field);
 
-	}
-	**/
 
 
 }
